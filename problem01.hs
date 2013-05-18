1031836a60fd1e4dbce9732bc3422ae7a686c838
weiningl <weiningltw@yahoo.com>
2013-05-18 15:17:42 -0700

    fixed 2 item corner case

--- problem01.hs
+++ problem01.hs
@@ -4,8 +4,23 @@ bowling = sum . concat . (take 10) . frames
 frames :: [Int] -> [[Int]]
 frames [] = []
 frames [x] = [[x]]
-frames [x, y] = [[x, y]]
+frames [x, y]
+  | x == 10 = [[x,y], [y]]
+  | otherwise = [[x,y]]
 frames (x1:x2:x3:xs)
   | x1 == 10 = [x1,x2,x3] : frames (x2:x3:xs)
   | x1 + x2 == 10 = [x1,x2,x3] : frames (x3:xs)
   | otherwise = [x1,x2] : frames (x3:xs)
+
+main :: IO ()
+main = do
+  let tests = [[],
+               [1,2,3,4],
+               [7,3,8,1],
+               [10,8,1],
+               [1,2,3],
+               [7,3],
+               [10],
+               [10,3]]
+      ans = map bowling tests
+  print ans
