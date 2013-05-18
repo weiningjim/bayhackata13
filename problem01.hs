ba4335fc83dfdb1d61ba34c7c782d24019347843
Jim Apple <jbapple@localhost>
2013-05-18 16:02:36 -0700

    top line score string

--- problem01.hs
+++ problem01.hs
@@ -6,14 +6,26 @@ bowling = sum . concat . (take 10) . frames
 
 -- problem 2: display scores
 scoreStr :: [Int] -> String
-scoreStr = concatMap frame2str . frames where
-  showNum 0 = "-"
-  showNum x = show x
-  frame2str (10:_) = "X "
-  frame2str [x] = showNum x ++ " "
-  frame2str (x:y:_)
-    | x + y == 10 = showNum x ++ "/"
-    | otherwise = showNum x ++ showNum y
+scoreStr xs = 
+  let ys = take 10 $ frames xs
+      (ps,qs) = splitAt 9 ys
+  in concatMap frame2str ps ++ concatMap lastFrame qs
+  
+showNum 0 = "-"
+showNum 10 = "X"
+showNum x = show x
+  
+frame2str (10:_) = (showNum 10)++" "
+frame2str [x] = showNum x ++ " "
+frame2str (x:y:_)
+  | x + y == 10 = showNum x ++ "/"
+  | otherwise = showNum x ++ showNum y
+                  
+lastFrame [x] = showNum x ++ "  "
+lastFrame [x,y] = concatMap showNum [x,y] ++ " "
+lastFrame [x,y,z] = concatMap showNum [x,y,z]
+    
+
 
 
 -- totalStr :: [Int] -> String
@@ -30,9 +42,7 @@ frames (x1:x2:x3:xs)
   | x1 + x2 == 10 = [x1,x2,x3] : frames (x3:xs)
   | otherwise = [x1,x2] : frames (x3:xs)
 
-main :: IO ()
-main = do
-  let tests = [[],
+tests = [[],
                [1,2,3,4],
                [7,3,8,1],
                [10,8,1],
@@ -40,12 +50,14 @@ main = do
                [7,3],
                [10],
                [10,3]]
+        
+main :: IO ()
+main = do
+  let 
       ans = map bowling tests
   print ans
   print $ map scoreStr tests
 
-
-perfect y =
-  not (null y) ==>
-  (maximum y <= 10) ==>
-  bowling y <= 300
+perfect y ys = 
+  ((maximum (y:ys)) <= 10) ==>
+  bowling (y:ys) <= 30*(1 + length ys)
